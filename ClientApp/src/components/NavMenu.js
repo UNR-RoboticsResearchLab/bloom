e838affcc48a7a22ba64d2509773ce75d926d0fc
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2025-10-20 10:23:09 -0700

    react 17 -> react 18

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,49 +1,38 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavMenu.css";
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+export default function NavMenu() {
 
-  constructor (props) {
-    super(props);
-
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">bloom</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+  return (
+    <header>
+      <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light><Container>
+        <Nav> 
+          <NavbarBrand tag={Link} to="/">
+            bloom
+          </NavbarBrand>         
+          <NavItem>
+            <NavLink tag={Link} to="/" className="text-dark">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/counter" className="text-dark">Counter</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/fetch-data" className="text-dark">Fetch data</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={Link} to="/sign-in" className="text-dark">Sign in</NavLink>
+          </NavItem>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
 }

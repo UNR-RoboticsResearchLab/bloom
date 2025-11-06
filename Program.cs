5228401dca416cf63c72d571d56eb3155cfd47b2
jrkre <joshuaknight@nevada.unr.edu>
2025-11-05 23:24:33 -0800

    swagger

--- Program.cs
+++ Program.cs
@@ -111,8 +111,8 @@ if (app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.MapOpenApi();
-    // app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {

14458d78bb2fdfa12da89e39659bf7f05346798b
illyaGavlovskyi <151693791+illyaGavlovskyi@users.noreply.github.com>
2026-02-08 13:25:10 -0800

    added forgot password

--- ClientApp/src/components/SignIn.js
+++ ClientApp/src/components/SignIn.js
@@ -72,7 +72,7 @@ export default function SignIn() {
   }
 
   return (
-    <div className="min-h-screen bg-white text-gray-900 flex flex-col px-6 py-12 lg:px-8 mt-[100px]">
+    <div className="min-h-screen bg-white text-gray-900 flex flex-col px-6 py-12 lg:px-8 mt-[100px] ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold tracking-tight">
           Sign in to your account
@@ -136,7 +136,7 @@ export default function SignIn() {
                 className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
                 aria-label="Toggle password visibility"
               >
-                {passwordShown ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+                {passwordShown ? <EyeSlashIcon className="h-5 w-5 text-gray-500" /> : <EyeIcon className="h-5 w-5 text-gray-500" />}
               </button>
             </div>
           </div>

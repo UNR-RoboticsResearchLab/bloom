3562e42ead3529d79bc84e5c1ec2ec1620f54e1d
mjknaefler <mjknaefler@gmail.com>
2026-03-02 23:52:41 -0800

    Undo last change, will come back to this

--- Robot/conversation_orchestrator.py
+++ Robot/conversation_orchestrator.py
@@ -114,17 +114,17 @@ class BloomOrchestrator:
         if platform.system() == 'Linux':
             self.browser_process = subprocess.Popen([
                 'chromium',
-                '--app=' + face_url,
-                '--start-fullscreen',
+                '--kiosk',
                 '--noerrdialogs',
                 '--disable-infobars',
                 '--no-first-run',
                 '--disable-session-crashed-bubble',
                 '--disable-restore-session-state',
+                '--disable-features=TranslateUI',
+                '--overscroll-history-navigation=0',
                 '--no-default-browser-check',
+                face_url
             ])
-            time.sleep(3)
-            subprocess.Popen(['xdotool', 'search', '--onlyvisible', '--class', 'chromium', 'windowactivate', 'key', 'F11'])
         else:
             # Mac/other - fallback to regular browser for development
             webbrowser.open(face_url)

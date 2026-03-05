c859768bd832f5307f54cf0a9fa1189848caf0a5
mjknaefler <mjknaefler@gmail.com>
2026-03-04 16:54:13 -0800

    Add engine modules as Python packages for ROS2

--- Robot/src/bloom_speech/setup.py
+++ Robot/src/bloom_speech/setup.py
@@ -5,7 +5,7 @@ package_name = 'bloom_speech'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(exclude=['test']),  
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),

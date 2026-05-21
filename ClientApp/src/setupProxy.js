e47050711391d714ae339ed9250c650c4348b555
jrkre <joshuaknight@unr.edu>
2026-05-21 15:38:12 -0700

    fix+demo link

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -1,17 +1,17 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware('/api', {
+    createProxyMiddleware({
+      pathFilter: '/api',
       target,
       changeOrigin: true,
       secure: false,
       cookieDomainRewrite: '',
       cookiePathRewrite: '/',
-      logLevel: 'debug',
     })
   );
 };

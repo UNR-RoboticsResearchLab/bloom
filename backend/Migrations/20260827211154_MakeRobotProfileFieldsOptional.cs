b18078162d8fdc06e33c97648506e89d6e392dd1
jrkre <joshuaknight@unr.edu>
2026-08-27 14:14:47 -0700

    optional fields instead of unclear form

---
+++ backend/Migrations/20260827211154_MakeRobotProfileFieldsOptional.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bloom.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeRobotProfileFieldsOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalityTrait",
+                table: "RobotProfiles",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "RobotProfiles",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "RobotProfiles",
+                keyColumn: "PersonalityTrait",
+                keyValue: null,
+                column: "PersonalityTrait",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalityTrait",
+                table: "RobotProfiles",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "RobotProfiles",
+                keyColumn: "Nickname",
+                keyValue: null,
+                column: "Nickname",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nickname",
+                table: "RobotProfiles",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
